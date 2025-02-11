e844d53638b4cb7c59f73bfa485704e91c213ef1
Gibson Smith <123333011+gdsmith1@users.noreply.github.com>
2025-02-11 13:01:04 -0800

    s3 can be deleted with objects still in it

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -1,12 +1,17 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   tags = var.tags
+  force_destroy = true
 }
 
 resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
     object_ownership = "BucketOwnerPreferred"
+  }
+
+  lifecycle {
+    prevent_destroy = false
   }
 }
 
